68772fcd1198242f48a6cb357e0509fe4b7ea7b3
Hiroshi Urabe (torounit) <mail@torounit.com>
2023-05-17 20:44:10 +0900

    change markup for dynamic block

--- packages/block-library/src/post-template/index.php
+++ packages/block-library/src/post-template/index.php
@@ -72,6 +72,7 @@ function render_block_core_post_template( $attributes, $content, $block ) {
 		$classnames .= ' has-link-color';
 	}
 
+	$wrapper_tag_name   = isset( $block->context['postsTagName'] ) ? $block->context['postsTagName'] : 'ul';
 	$wrapper_attributes = get_block_wrapper_attributes( array( 'class' => trim( $classnames ) ) );
 
 	$content = '';
@@ -110,7 +111,8 @@ function render_block_core_post_template( $attributes, $content, $block ) {
 	wp_reset_postdata();
 
 	return sprintf(
-		'<ul %1$s>%2$s</ul>',
+		'<%1$s %2$s>%3$s</%1$s>',
+		tag_escape( $wrapper_tag_name ),
 		$wrapper_attributes,
 		$content
 	);
